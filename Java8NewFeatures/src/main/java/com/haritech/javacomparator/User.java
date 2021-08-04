0bf0295ce203a086711380e388edf16cee7b159a
harikd <88421026+harikd@users.noreply.github.com>
2021-08-04 12:31:40 +0530

    added location in user

--- Java8NewFeatures/src/main/java/com/haritech/javacomparator/User.java
+++ Java8NewFeatures/src/main/java/com/haritech/javacomparator/User.java
@@ -3,10 +3,12 @@ package com.haritech.javacomparator;
 public class User {
  String username;
  String pwd;
-public User(String username, String pwd) {
+String location;	
+public User(String username, String pwd,String location) {
 	super();
 	this.username = username;
 	this.pwd = pwd;
+	this.location=location;
 }
  
  
